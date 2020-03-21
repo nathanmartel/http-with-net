da93a8647f5dbadbbd938dde175b81cf54c19406
Nathan Martel <nathanmartel@gmail.com>
2020-03-20 17:17:56 -0700

    correct starter code, that helps

---
+++ sandbox.js
@@ -0,0 +1,3 @@
+const str = 'hello everyone. how are you';
+
+console.log(str.split(''));
--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
-
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7890;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+app.listen(7890, () => {
+  console.log('Application started!');
 });
